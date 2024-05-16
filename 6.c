7b15c5443bc3330a70fd66cf314bae065e44eeeb
Caio <168545443+CaioUgo@users.noreply.github.com>
2024-05-16 07:55:06 -0300

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+ //Atividade 6
+
+	int main () {
+
+    float pesoKg, resultado;
+    
+    	printf("Informe o peso em Kg: ");
+    	  scanf("%f", &pesoKg);    	  
+    	  
+    		resultado = pesoKg * 1000;
+    		
+    			printf("O peso em gramas = %.2f", resultado);
+ 	}

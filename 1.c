17632c943874440f2c7d7832bf6d5db40fa88429
Caio <168545443+CaioUgo@users.noreply.github.com>
2024-05-16 07:46:29 -0300

    1.c

---
+++ 1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+//Atividade 1
+
+ int main() {
+    
+     //Declaração de variáveis
+     
+   
+     int nota1, nota2, nota3, nota4,totalNotas; 
+      
+      
+    
+     
+     //Exibindo as váriaveis
+    printf("Digite a primeira nota: ");
+     scanf("%d", &nota1);
+ 
+    printf("Digite a segunda nota: ");
+     scanf("%d", &nota2);
+
+    printf("Digite a terceira nota: ");
+     scanf("%d", &nota3);
+
+    printf("Digite a quarta nota: ");
+     scanf("%d", &nota4);
+     
+         
+
+	     totalNotas = nota1 + nota2 + nota3 + nota4;
+    
+     
+     
+   			 printf("Total do aluno = %d",totalNotas);
+
+
+ }

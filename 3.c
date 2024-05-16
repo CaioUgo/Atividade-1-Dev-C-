f50ec7dcffbb38e76e2d6ee52740a951fdca7ee5
Caio <168545443+CaioUgo@users.noreply.github.com>
2024-05-16 07:50:58 -0300

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+//Atividade 3
+
+ int main() {
+    
+     //Declaração de variáveis
+     
+   
+     int largura, comprimento, areaRetangulo; 
+            
+     
+     //Exibindo as váriaveis
+    printf("Informe a largura: ");
+     scanf("%d", &largura);
+ 
+    printf("Informe o comprimento: ");
+     scanf("%d", &comprimento);
+
+     		areaRetangulo = largura * comprimento;
+
+     
+     
+   			 printf("A area do retangulo = %d ",areaRetangulo);
+   			 printf("metros quadrados");
+
+
+ }

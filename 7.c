eb88869fa4b70f7b541177c15b2d5acd3c72d5b3
Caio <168545443+CaioUgo@users.noreply.github.com>
2024-05-16 08:05:51 -0300

    Create 7.c

---
+++ 7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+ //Atividade 7
+
+	int main () {
+
+    float pesoKg, resultado;
+    
+    	printf("Informe o peso em Kg: ");
+    	  scanf("%f", &pesoKg);    	  
+    	  
+    		resultado = pesoKg * 45;
+    		
+    			printf("O preco da comida R$ %.2f", resultado);
+    		}

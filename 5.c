dbd97f6868eacbb25291c5b57bd6742c16ca552f
Caio <168545443+CaioUgo@users.noreply.github.com>
2024-05-16 07:53:26 -0300

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+ //Atividade 5
+
+	int main () {
+
+    float primeiroValor, segundoValor, resultado;
+    
+    	printf("Informe o primeiro valor: ");
+    	  scanf("%f", &primeiroValor);
+    	  
+		printf("Informe o segundo valor: ");
+    	  scanf("%f", &segundoValor);
+    	  
+    		resultado = primeiroValor / segundoValor;
+    		
+    			printf("A divsao = %.2f", resultado);
+    	  
+
+
+}

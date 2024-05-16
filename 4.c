97edad0871e885d93ab2244d27648a2986ae84d0
Caio <168545443+CaioUgo@users.noreply.github.com>
2024-05-16 07:52:07 -0300

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+//Atividade 4
+
+ int main() {
+    
+     //Declaração de variáveis
+     
+   
+     int numero1,numero2,diferenca; 
+            
+     
+     //Exibindo as váriaveis
+
+		
+	printf("Informe o primeiro numero : ");
+     scanf("%d", &numero1);
+    
+	printf("Informe o segundo numero : ");
+     scanf("%d", &numero2); 
+    
+        diferenca = numero1 - numero2;
+
+
+    
+   			 printf("A diferenca dos numeros = %d ",diferenca);
+   			 
+
+
+ }
